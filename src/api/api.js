0994cc180ef8d3c6491ae5d136b3b7cf78bafb0b
Vlad Lositsky <vladyslav.lositsky@gmail.com>
2022-04-27 20:46:34 +0200

    Lesson 63

--- src/api/api.js
+++ src/api/api.js
@@ -1,19 +1,28 @@
 import axios from 'axios'
 
-
-const instance = axios.create({
+const axiosInstance = axios.create({
   baseURL: 'https://social-network.samuraijs.com/api/1.0/',
   withCredentials: true,
-  headers: {
-    "API-KEY": "514803d4-d50a-4581-9403-b2efec04021f"
-  }
+  headers: { "API-KEY": "d7d8b2e7-6066-4cde-91e9-656d90847c44" }
 })
 
+export const authAPI = {
+  login: () => {
+    return axiosInstance.get(`auth/me`)
+      .then(response => response.data)
+  }
+}
 export const usersAPI = {
-  getUsers(currentPage, pageSize) {
-    return instance.get(`users?page=${ currentPage }&count=${ pageSize }`, {
-      withCredentials: true
-    })
+  getUsers: (currentPage = 1, pageSize = 10) => {
+    return axiosInstance.get(`users?page=${ currentPage }&count=${ pageSize }`)
+      .then(response => response.data)
+  },
+  follow: id => {
+    return axiosInstance.post(`follow/${ id }`)
+      .then(response => response.data)
+  },
+  unfollow: id => {
+    return axiosInstance.delete(`follow/${ id }`)
       .then(response => response.data)
   }
 }

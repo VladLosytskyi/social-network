2ab32b89a639ea98947de16683da762a46454bcd
Vlad Lositsky <vladyslav.lositsky@gmail.com>
2022-06-30 09:09:48 +0200

    Added types for common

--- src/components/common/Paginator/Paginator.tsx
+++ src/components/common/Paginator/Paginator.tsx
@@ -12,44 +12,53 @@ interface PaginatorProps {
   portionSize?: number
 }
 
-const Paginator: FC<PaginatorProps> =
-  ({ totalItemsCount, pageSize, currentPage, onPageChange, portionSize = 10 }) => {
-    const pagesCount = Math.ceil(totalItemsCount / pageSize)
-    const portionCount = Math.ceil(pagesCount / portionSize)
-    const [portionNumber, setPortionNumber] = useState(1)
-    const leftPortionPageNumber = (portionNumber - 1) * portionSize + 1
-    const rightPortionPageNumber = portionNumber * portionSize
+const Paginator: FC<PaginatorProps> = ({
+                                         totalItemsCount,
+                                         pageSize,
+                                         currentPage,
+                                         onPageChange,
+                                         portionSize = 10
+                                       }) => {
 
-    const pages: number[] = []
-    for (let i = 1; i <= pagesCount; i++) {
-      pages.push(i)
-    }
+  const pagesCount = Math.ceil(totalItemsCount / pageSize)
+  const portionCount = Math.ceil(pagesCount / portionSize)
 
-    return (
-      <div className={ classes.paginator }>
-        { portionNumber > 1 && <FontAwesomeIcon icon={ faArrowLeftLong }
-                                                className={ classes.arrow }
-                                                onClick={ () => setPortionNumber(portionNumber - 1) } />
-        }
-        <div className={ classes.pageNumbers }>
-          {
-            pages
-              .filter(pageNumber => pageNumber >= leftPortionPageNumber && pageNumber <= rightPortionPageNumber)
-              .map(pageNumber => {
-                return (
-                  <span className={ `${ currentPage === pageNumber && classes.selectedPage } ${ classes.pageNumber }` }
-                        onClick={ () => onPageChange(pageNumber) }
-                        key={ pageNumber }>{ pageNumber }</span>
-                )
-              })
-          }
-        </div>
-        { portionCount > portionNumber && <FontAwesomeIcon icon={ faArrowRightLong }
-                                                           className={ classes.arrow }
-                                                           onClick={ () => setPortionNumber(portionNumber + 1) } />
+  const [portionNumber, setPortionNumber] = useState(1)
+
+  const leftPortionPageNumber = (portionNumber - 1) * portionSize + 1
+  const rightPortionPageNumber = portionNumber * portionSize
+
+
+  const pages: number[] = []
+  for (let i = 1; i <= pagesCount; i++) {
+    pages.push(i)
+  }
+
+  return (
+    <div className={ classes.paginator }>
+      { portionNumber > 1 && <FontAwesomeIcon icon={ faArrowLeftLong }
+                                              className={ classes.arrow }
+                                              onClick={ () => setPortionNumber(portionNumber - 1) } />
+      }
+      <div className={ classes.pageNumbers }>
+        {
+          pages
+            .filter(pageNumber => pageNumber >= leftPortionPageNumber && pageNumber <= rightPortionPageNumber)
+            .map(pageNumber => {
+              return (
+                <span className={ `${ currentPage === pageNumber && classes.selectedPage } ${ classes.pageNumber }` }
+                      onClick={ () => onPageChange(pageNumber) }
+                      key={ pageNumber }>{ pageNumber }</span>
+              )
+            })
         }
       </div>
-    )
-  }
+      { portionCount > portionNumber && <FontAwesomeIcon icon={ faArrowRightLong }
+                                                         className={ classes.arrow }
+                                                         onClick={ () => setPortionNumber(portionNumber + 1) } />
+      }
+    </div>
+  )
+}
 
 export default Paginator
--- src/components/common/Preloader/Preloader.tsx
+++ src/components/common/Preloader/Preloader.tsx
@@ -1,6 +1,8 @@
-import preloader from '../../../assets/images/preloader.svg'
+import { FC } from 'react'
+// @ts-ignore
+const preloader = require('../../../assets/images/preloader.svg') as string
 
-const Preloader = () => {
+const Preloader: FC = () => {
   return (
     <div style={ { display: 'flex', justifyContent: 'center', alignItems: 'center', maxWidth: '1000px' } }>
       <img src={ preloader } alt='Preloader' />

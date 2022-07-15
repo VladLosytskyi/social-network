43ede0b53375dfa354a1ec5c96ca20fccae24972
Vlad Lositsky <vladyslav.lositsky@gmail.com>
2022-07-15 12:36:51 +0200

    Updated Welcome Page

--- src/components/Welcome/Welcome.tsx
+++ src/components/Welcome/Welcome.tsx
@@ -10,18 +10,10 @@ const Welcome: FC = () => {
                                href="https://reactjs.org/"
                                target="_blanc">ReactJS</a> project</h1>
       <div>
-        <p>Unfortunately, the <a className={ classes.blueText }
-                                 href="https://en.wikipedia.org/wiki/API"
-                                 target="_blanc">API</a> that my project uses does not support sign up functionality.
-        </p>
-        <p>Therefore, I have to ask you to visit <a className={ classes.blueText }
-                                                    href="https://social-network.samuraijs.com/signUp"
-                                                    target="_blanc">API's Website</a> to register.
-        </p>
-        <p>After registration, please return on <NavLink to="/login" className={ classes.blueText }>Login Page</NavLink>
-          of this web application and login here.</p>
-        <p>Then the functionality of the site will be available to you as a registered user.</p>
-        <p>I am sorry for the inconvenience.</p>
+        <p>To test the functionality of this Web Application, please
+          <NavLink to='/login' className={ classes.blueText }> log in </NavLink>using this test data:</p>
+        <p><span className={ classes.blueText }>Email:</span> free@samuraijs.com</p>
+        <p><span className={ classes.blueText }>Password:</span> free</p>
       </div>
     </section>
   )

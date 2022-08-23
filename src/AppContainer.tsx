89a391b3811e1acfe5ea62a03ad12ee9f3901e81
Vlad Lositsky <vladyslav.lositsky@gmail.com>
2022-08-23 17:03:03 +0200

    Changed HashRouter on BrowserRouter

--- src/AppContainer.tsx
+++ src/AppContainer.tsx
@@ -1,15 +1,15 @@
 import { Provider } from 'react-redux'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import store from './redux/store'
 
 const AppContainer = () => {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Provider store={ store }>
         <App />
       </Provider>
-    </HashRouter>
+    </BrowserRouter>
   )
 }
 
